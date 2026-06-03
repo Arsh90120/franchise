aeb49a5c8cb0d45bd2e97e0da7fc91c7f478e91d
Arsh90120 <scarletspeedster2131@gmail.com>
2026-06-03 18:55:48 -0400

    fix: remove next.config.ts (replaced with next.config.mjs)

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['cdn.nba.com', 'ak-static.cms.nba.com'],
-  },
-};
-
-export default nextConfig;
